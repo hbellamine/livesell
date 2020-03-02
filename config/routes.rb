9dcae53ef5b5792384b43434f5696d6bb2a3514d
hamza bellamine <hamza.bellamine@gmail.com>
2020-03-02 16:28:45 +0000

    controllers-models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/new'
+  get 'orders/create'
+  get 'orders/edit'
+  get 'orders/update'
+  get 'orders/destroy'
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+  get 'sessions/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/edit'
+  get 'sessions/update'
+  get 'sessions/destroy'
   devise_for :users
   root to: 'pages#home'
+
+
+  resources :users do
+      resources :sessions, only:[:index,:show,:new,:create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

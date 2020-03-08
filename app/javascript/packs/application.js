ebc32a33846ea9e9609062797c641056f7808cd7
hamza bellamine <hamza.bellamine@gmail.com>
2020-03-08 14:14:39 +0100

    loginfacebook

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import "bootstrap";
 
 
 
-var id = 537938066831571
+var id = 1105476746477957
 var url = `https://streaming-graph.facebook.com/${id}/live_comments?access_token=EAASlfcqhS6MBADeZCIYMk6vaAb966ghu3NMcyJZCZAmObHHTQ2uwCmYZAUlWSaxuRgMBaoMoBSg10WyJrZCTbJjYCuTyOKbha8RNOj924KjVEJqficwAeFoFtzDK3TmuhZBrblwuRKiw7ujXZCcgXY2oMpejGRJt1OTyp6m8hqPAOutx3TiXK0N4egZCZARGGtc8ZD&comment_rate=one_per_two_seconds&fields=from{name,id},message`
 var source = new EventSource(url);
 

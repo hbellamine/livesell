36ab2f17b2032b8763314b23ab25e7a209b002d9
hamza bellamine <hamza.bellamine@gmail.com>
2020-03-10 14:19:52 +0000

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,8 +44,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://livecastsales.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ "http://livecastsales.herokuapp.com/", "http://livecastsales.herokuapp.com/" ]
+  config.action_cable.url = 'ws://livecastsales.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ "http://livecastsales.herokuapp.com/", "https://livecastsales.herokuapp.com/" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

f6a157dfd5f645c0aba4c4758f6e5e125268959f
hamza bellamine <hamza.bellamine@gmail.com>
2020-07-13 19:24:52 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  #   sessions: 'users/sessions',
+  #   omniauth_callbacks: 'users/omniauth_callbacks'
+  # }
 root to: 'welcome#index'
   resources :stores, only: [:edit, :update]
   get 'stores/:id', to: 'stores#show', as: 'mystore'
